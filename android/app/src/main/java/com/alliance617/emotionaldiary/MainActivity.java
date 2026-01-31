99b18ed02c91e29c4eeee16ee93c4114cd2dbcea
JongHyeon Moon <zin354@gmail.com>
2026-01-31 11:24:51 +0900

    feat: capacitor 래핑 및 ios 배포 파이프라인 추가

---
+++ android/app/src/main/java/com/alliance617/emotionaldiary/MainActivity.java
@@ -0,0 +1,5 @@
+package com.alliance617.emotionaldiary;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.alliance617.emotionaldiary",
+  appName: "bible-cbt-diary",
+  webDir: "out",
+};
+
+export default config;
